d699e1a1f9648b2dd03c9abdc7e1957e612893f6
THOMAS DAGHELET <thomas.daghelet@adeo.com>
2025-05-28 13:34:03 +0200

    feat(): wip

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const core = require('@actions/core');
+const glob = require('@actions/glob');
 
 async function run() {
   try {
     core.info('Search for missing package-lock.json files');
+
+    const globber = glob.create('package.json', globOptions)
+    for await (const file of globber.globGenerator()) {
+      console.log(file)
+    }
   }
   catch (error) {
     core.setFailed(error.message);
